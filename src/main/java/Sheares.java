4e6529cf49b6e9939755dca7ad8dc8749fe9e236
Shearer1509 <shearer1509@gmail.com>
2025-08-13 14:37:24 +0800

    level-0 commit

---
+++ src/main/java/Sheares.java
@@ -0,0 +1,9 @@
+public class Sheares {
+    public static void main(String[] args) {
+        String zero = "_____________________________\n";
+        String first = "Hello! I'm Sheares\n";
+        String second = "What can i do for you?\n";
+        String third = "Bye. Hope to see you again soon!\n";
+        System.out.println(zero + first + second + zero + third + zero);
+    }
+}
