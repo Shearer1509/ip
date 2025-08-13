68fdedae23b8167d1f4cb7528fc599e046360343
Shearer1509 <shearer1509@gmail.com>
2025-08-13 15:25:37 +0800

    commited level 1

--- src/main/java/Sheares.java
+++ src/main/java/Sheares.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Sheares {
     public static void main(String[] args) {
-        String zero = "_____________________________\n";
-        String first = "Hello! I'm Sheares\n";
-        String second = "What can i do for you?\n";
-        String third = "Bye. Hope to see you again soon!\n";
-        System.out.println(zero + first + second + zero + third + zero);
+        Scanner sc = new Scanner(System.in);
+        String zero = "    _____________________________\n";
+        String first = "    Hello! I'm Sheares\n";
+        String second = "    What can i do for you?\n";
+        String third = "    Bye. Hope to see you again soon!\n";
+        System.out.println(zero + first + second + zero);
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                System.out.println(zero);
+                break;
+            } else {
+                System.out.println(zero);
+                System.out.println("    " + input);
+                System.out.println(zero);
+            }
+        }
+        System.out.println(third + zero);
     }
 }
