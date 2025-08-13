da18fa4a2bc12f948fff463b4b24294153f7162e
Shearer1509 <shearer1509@gmail.com>
2025-08-13 15:43:20 +0800

    commited level 2

--- src/main/java/Sheares.java
+++ src/main/java/Sheares.java
@@ -1,24 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sheares {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String zero = "    _____________________________\n";
-        String first = "    Hello! I'm Sheares\n";
-        String second = "    What can i do for you?\n";
-        String third = "    Bye. Hope to see you again soon!\n";
-        System.out.println(zero + first + second + zero);
+        String[] ans = new String[100];
+        int numOfTasks = 0;
+        String zero = "    _____________________________";
+        String first = "    Hello! I'm Sheares";
+        String second = "    What can i do for you?";
+        String third = "    Bye. Hope to see you again soon!";
+        System.out.println(zero);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(zero);
         while (sc.hasNext()) {
-            String input = sc.next();
+            String input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println(zero);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(zero);
+                for (int i = 0; i < numOfTasks; i++) {
+                    System.out.println("    " + (i+1) + ". " + ans[i]);
+                }
+                System.out.println(zero);
             } else {
+                ans[numOfTasks] = input;
                 System.out.println(zero);
-                System.out.println("    " + input);
+                System.out.println("    added: " + input);
                 System.out.println(zero);
+                numOfTasks++;
             }
         }
-        System.out.println(third + zero);
+        System.out.println(third);
+        System.out.println(zero);
     }
 }
