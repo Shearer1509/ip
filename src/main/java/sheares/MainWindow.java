af931f73f68ba67230e4538ca391070b1ed1a76c
Shearer1509 <shearer1509@gmail.com>
2025-08-29 21:23:57 +0800

    Change images for chatbot

--- src/main/java/sheares/MainWindow.java
+++ src/main/java/sheares/MainWindow.java
@@ -26,8 +26,8 @@ public class MainWindow extends AnchorPane {
 
     private Sheares sheares;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/userImage.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/dukeImage.png"));
 
     /**
      * Initializes the entry layout + initial message of the chatbot
